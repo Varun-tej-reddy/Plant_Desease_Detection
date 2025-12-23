4a5721878ab332d002345da01721d44b19b72422
Varun-tej-reddy <pvaruntejreddy@gmail.com>
2025-12-23 18:10:35 +0530

    Remove sensitive password from db_config.py

--- db_config.py
+++ db_config.py
@@ -4,7 +4,7 @@ def get_db_connection():
     connection = mysql.connector.connect(
         host="localhost",
         user="root",
-        password="Varun@6688",  # your password
+        password="    ",  # your password
         database="plant_disease_app"
     )
     return connection
